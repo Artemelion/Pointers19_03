f967993f61b8498642d152b6181066c871272912
Artemeleon Morarevich <artyommorar@gmail.com>
2025-03-26 08:39:01 +0200

    Added Header.h and Main.cpp

---
+++ Header.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include <cmath>
+
+// 1) Increments the value pointed to by ptr by n (default is 1)
+void my_increment(int* ptr, int n = 1) {
+    *ptr += n;
+}
+
+// 2) Multiplies values pointed to by a and b, stores result in result
+void multiply(int* a, int* b, int* result) {
+    *result = (*a) * (*b);
+}
+
+// 3) Negates the value pointed to by ptr
+void negate(double* ptr) {
+    *ptr = -(*ptr);
+}
+
+// 4) Sets the value pointed to by ptr to newValue
+void setValue(int* ptr, int newValue) {
+    *ptr = newValue;
+}
+
+// 5) Concatenates digits from a and b, stores result in result
+// e.g., a = 12, b = 34 → result = 1234
+void concatenateDigits(int* a, int* b, int* result) {
+    int temp = *b;
+    int digits = 0;
+
+    while (temp != 0) {
+        temp /= 10;
+        digits++;
+    }
+
+    *result = (*a) * pow(10, digits) + (*b);
+}
---
+++ Main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "Header.h"
+using namespace std;
+
+int main() {
+    // Test 1: my_increment
+    int x = 5;
+    my_increment(&x);
+    cout << "Incremented (default): " << x << endl;
+
+    my_increment(&x, 10);
+    cout << "Incremented (+10): " << x << endl;
+
+    // Test 2: multiply
+    int a = 3, b = 4, res = 0;
+    multiply(&a, &b, &res);
+    cout << "Multiply: " << res << endl;
+
+    // Test 3: negate
+    double d = 3.14;
+    negate(&d);
+    cout << "Negated: " << d << endl;
+
+    // Test 4: setValue
+    int val = 0;
+    setValue(&val, 42);
+    cout << "Set value: " << val << endl;
+
+    // Test 5: concatenateDigits
+    int num1 = 12, num2 = 34, combined = 0;
+    concatenateDigits(&num1, &num2, &combined);
+    cout << "Concatenated: " << combined << endl;
+
+    return 0;
+}
